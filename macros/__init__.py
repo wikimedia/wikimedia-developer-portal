a2f95f36bf8bc658a19dd9b7f90f64c5ec05b468
Bryan Davis <bd808@wikimedia.org>
2021-11-01 17:08:35 -0600

    mkdocs: add mkdocs-macros-plugin
    Add the macros plugin which enables using jinja2 in markdown source
    files and provides hooks to inject variables, macros, and filters into
    the jinja2 rendering context.
    
    This plugin is injected into the mkdocs plugin list before mdpo which
    means that mdpo sees the output of macros when collecting translation
    units from the English source. This will allow us to use macros to
    generate translation units, but comes with a side effect that some
    output may break mdpo. A really easy example of this is that adding
    a `{{ macros_info() }}` block produces output which causes mdpo's
    initialization code to fall into a recursion loop which eventually
    crashes mkdocs.

---
+++ macros/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Wikimedia Developer Portal.
+#
+# Wikimedia Developer Portal is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Wikimedia Developer Portal is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Wikimedia Developer Portal.  If not, see <http://www.gnu.org/licenses/>.
+
+def define_env(env):
+    """Setup local variables, macros, and filters for mkdocs-macros-plugin."""
+
+    @env.macro
+    def say_hello(s):
+        return "Hello {}".format(s)
